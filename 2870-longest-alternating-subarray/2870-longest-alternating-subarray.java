04f70455adc059d32b8963a1f6db5af5cfad1007
HariniSenthilkumar2305 <162793945+HariniSenthilkumar2305@users.noreply.github.com>
2025-10-03 22:20:35 +0530

    Time: 195 ms (3.93%), Space: 55.2 MB (6.11%) - LeetHub

--- 2870-longest-alternating-subarray/2870-longest-alternating-subarray.java
+++ 2870-longest-alternating-subarray/2870-longest-alternating-subarray.java
@@ -1,51 +1,50 @@
 class Solution {
     public int alternatingSubarray(int[] nums) {
-        List<List<Integer>> l = new ArrayList<>();
-
-        // generate all subarrays
-        for (int i = 0; i < nums.length - 1; i++) {
-            List<Integer> a = new ArrayList<>();
+        List<List<Integer>>l=new ArrayList<>();
+        for(int i=0;i<nums.length-1;i++){
+            List<Integer>a=new ArrayList<>();
             a.add(nums[i]);
-            for (int j = i + 1; j < nums.length; j++) {
+            for(int j=i+1;j<nums.length;j++){
                 a.add(nums[j]);
-                l.add(new ArrayList<>(a)); // FIX: copy, not same reference
+                l.add(new ArrayList<>(a));
             }
         }
-
-        List<List<Integer>> res = new ArrayList<>();
-        for (int i = 0; i < l.size(); i++) {
-            List<Integer> b = new ArrayList<>(l.get(i));
-            boolean valid = true;
-
-            // check alternating condition
-            for (int j = 1; j < b.size(); j++) {
-                int diff = b.get(j) - b.get(j - 1);
-                if (j % 2 != 0) {
-                    // odd index: must be +1
-                    if (diff != 1) {
-                        valid = false;
+        List<List<Integer>>res=new ArrayList<>();
+        for(int i=0;i<l.size();i++){
+            List<Integer>b=new ArrayList<>(l.get(i));
+            boolean oddidx=false;
+            boolean evnidx=false;
+            boolean valid=true;
+            for(int j=1;j<b.size();j++){
+                int diff=b.get(j)-b.get(j-1);
+                if(j%2!=0){
+                    if(diff==1){
+                        oddidx=true;
+                    }
+                    else{
+                        valid=false;
                         break;
                     }
-                } else {
-                    // even index: must be -1
-                    if (diff != -1) {
-                        valid = false;
+                }
+                else{
+                     if(diff==-1){
+                        evnidx=true;
+                    }
+                    else{
+                        valid=false;
                         break;
                     }
                 }
             }
-
-            if (valid && b.size() > 1) {
+            if(valid && b.size()>1){
                 res.add(b);
             }
         }
-
-        int maxlen = -1; // If no alternating subarray found
-        for (int i = 0; i < res.size(); i++) {
-            List<Integer> w = new ArrayList<>(res.get(i));
-            maxlen = Math.max(maxlen, w.size());
+        int maxlen=-1;
+        for(int i=0;i<res.size();i++){
+            List<Integer>w=new ArrayList<>(res.get(i));
+            maxlen=Math.max(maxlen,w.size());
         }
-
         return maxlen;
     }
 }
