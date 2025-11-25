abf72b212f9a6a78cbc457364b24d8486a7093e8
HariniSenthilkumar2305 <162793945+HariniSenthilkumar2305@users.noreply.github.com>
2025-11-25 17:15:53 +0530

    Time: 3 ms (40.64%), Space: 43.2 MB (11.31%) - LeetHub

--- 2451-odd-string-difference/2451-odd-string-difference.java
+++ 2451-odd-string-difference/2451-odd-string-difference.java
@@ -1,67 +1,48 @@
 class Solution {
-
-    public String oddString(String[] words) {
-
-        HashMap<String, List<Integer>> mp = new HashMap<>();
-        List<List<Integer>> l = new ArrayList<>();
-
-        // Build difference lists
-        for (int i = 0; i < words.length; i++) {
-            String w = words[i];
-            List<Integer> a = new ArrayList<>();
-
-            for (int j = 0; j < w.length() - 1; j++) {
-                int d = (w.charAt(j + 1) - 'a') - (w.charAt(j) - 'a');
-                a.add(d);
-            }
-
-            mp.put(w, a);
-            l.add(a);
+    public boolean compareList(List<Integer> a,List<Integer> b){
+        if(a.size()!=b.size()){
+            return false;
         }
-
-        // Compare each pattern with others
-        for (int i = 0; i < l.size(); i++) {
-
-            List<Integer> x = l.get(i);
-            boolean val = false;
-
-            for (int j = 0; j < l.size(); j++) {
-
-                if (i != j) {
-
-                    List<Integer> y = l.get(j);
-
-                    val = compareLists(x, y);  // correct comparison
-
-                    if (val)
-                        break;
-                }
-            }
-
-            if (!val) {
-                // find which key has this value
-                for (String key : mp.keySet()) {
-                    if (compareLists(mp.get(key), x)) {
-                        return key;
-                    }
-                }
-            }
-        }
-
-        return "";
-    }
-
-    // Manual list comparison — same logic as yours
-    public boolean compareLists(List<Integer> a, List<Integer> b) {
-
-        if (a.size() != b.size()) return false;
-
-        for (int i = 0; i < a.size(); i++) {
-            if (!a.get(i).equals(b.get(i))) {
+        for(int i=0;i<a.size();i++){
+            if(!a.get(i).equals(b.get(i))){
                 return false;
             }
         }
-
         return true;
+    }
+    public String oddString(String[] words) {
+        HashMap<String,List<Integer>>mp=new HashMap<>();
+        List<List<Integer>>l=new ArrayList<>();
+        for(int i=0;i<words.length;i++){
+            String w=words[i];
+            List<Integer>a=new ArrayList<>();
+            for(int j=0;j<w.length()-1;j++){
+               int l1=(w.charAt(j+1)-'a')-(w.charAt(j)-'a');
+               a.add(l1);
+            }
+            mp.put(w,a);
+            l.add(a);
+        }
+        for(int i=0;i<l.size();i++){
+            List<Integer>x=l.get(i);
+            boolean val=false;
+            for(int j=0;j<l.size();j++){
+                if(i!=j){
+                List<Integer>y=l.get(j);
+                val=compareList(x,y);
+                if(val){
+                    break;
+                }
+            }
+        }
+        if(!val){
+            for(String key:mp.keySet()){
+                if(compareList(mp.get(key),x)){
+                    return key;
+                }
+            }
+        }
+        }
+        return "";
     }
 }
