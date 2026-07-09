586951f5f24d4fd1543a7235ed0605445022bacd
HariniSenthilkumar2305 <162793945+HariniSenthilkumar2305@users.noreply.github.com>
2026-07-09 19:07:50 +0530

    Time: 54 ms (7.04%), Space: 47.8 MB (15.55%) - LeetHub

--- 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.java
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.java
@@ -1,29 +1,27 @@
-import java.util.*;
-
 class Solution {
     public int countCharacters(String[] words, String chars) {
-        int sum = 0;
-        HashMap<Character, Integer> hp = new HashMap<>();
-        for (int i = 0; i < chars.length(); i++) {
-            hp.put(chars.charAt(i), hp.getOrDefault(chars.charAt(i), 0) + 1);
+      int sum=0;
+      HashMap<Character,Integer>hp=new HashMap<>();
+      for(int i=0;i<chars.length();i++){
+        hp.put(chars.charAt(i),hp.getOrDefault(chars.charAt(i),0)+1);
+      }
+      for(int i=0;i<words.length;i++){
+        String w=words[i];
+        HashMap<Character,Integer>h=new HashMap<>();
+        for(int j=0;j<w.length();j++){
+            h.put(w.charAt(j),h.getOrDefault(w.charAt(j),0)+1);
         }
-        for (int i = 0; i < words.length; i++) {
-            String w = words[i];
-            HashMap<Character, Integer> h = new HashMap<>();
-            for (int j = 0; j < w.length(); j++) {
-                h.put(w.charAt(j), h.getOrDefault(w.charAt(j), 0) + 1);
-            }
-            boolean val = true;
-            for (Map.Entry<Character, Integer> m : h.entrySet()) {
-                if (!hp.containsKey(m.getKey()) || hp.get(m.getKey()) < m.getValue()) {
-                    val = false;
-                    break;
-                }
-            }
-            if (val) {
-                sum += w.length();
+        boolean val=true;
+        for(Map.Entry<Character,Integer>m:h.entrySet()){
+            if(!hp.containsKey(m.getKey()) || hp.get(m.getKey())<m.getValue()){
+                val=false;
+                break;
             }
         }
-        return sum;
+        if(val){
+            sum+=w.length();
+        }
+      }
+      return sum;
     }
 }
